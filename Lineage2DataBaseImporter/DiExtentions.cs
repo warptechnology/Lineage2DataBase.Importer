bd83cb7d76678243f7ac9cf1df436a1722615dd8
Anatoly <warptechnology@gmail.com>
2025-08-09 13:50:04 +0300

    init

---
+++ Lineage2DataBaseImporter/DiExtentions.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Lineage2DataBase.Api
+{
+    /// <summary>
+    /// Предоставляет методы расширения для регистрации сервисов Lineage2DataBase API в контейнере зависимостей.
+    /// </summary>
+    public static class DiExtensions
+    {
+       
+    }
+}
